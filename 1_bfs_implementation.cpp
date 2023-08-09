980e6def3af2f51f1c22f9166e3f774f4bf7347e
Ibnul Ferdous <ibnul04@gmail.com>
2023-08-09 11:59:17 +0600

    bfs level showing

--- 1_bfs_implementation.cpp
+++ 1_bfs_implementation.cpp
@@ -23,19 +23,44 @@ int main(void)
         adl[v].push_back(u);
     }
 
+    bfs(1);
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout<<"Vextex "<< i << " : " << level[i] << endl;
+    }
 
     return 0;
 }
 
 void bfs(int s)
 {
+    // Step 1: take a queue and push root/first vertex into the queue
+    // Step 2: queue empty na howa porjonto loop chalao
+    // Step 3: queue theke front ta k ber koro and kothao save kore rakho
+    // Step 4: front diye jabotiyo kaaj shesh koro
+    // Step 5: front er children/neighbor der k queue te push koro
+
+    // step 1
     queue<int> q;
     visited[s] = true;
-
-    // Step 1: push root/first vertex into the queue
-    // Step 2: queue theke front ta k ber koro and kothao save kore rakho
-    // Step 3: front diye jabotiyo kaaj shesh koro
-    // Step 4: front er children/neighbor der k queue te push koro
-
     q.push(s);
+
+    // step 2
+    while (!q.empty())
+    {
+        // setp 3
+        int u = q.front();
+        q.pop();
+
+        // step 4
+        for (int v : adl[u])
+        {
+            if (visited[v]) continue;
+            // step 5
+            q.push(v);
+            visited[v] = true;
+            level[v] = level[u] + 1;
+        }
+    }
 }
