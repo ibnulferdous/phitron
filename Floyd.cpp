a8d95a6bd5dc02c58cd0f994715a7724f7ace45e
Ibnul Ferdous <ibnul04@gmail.com>
2023-08-28 15:33:40 +0600

    3rd problem- got stuck

--- Floyd.cpp
+++ Floyd.cpp
@@ -4,49 +4,50 @@ using namespace std;
 
 int main(void)
 {
-    int n;
+    int n, max_distance = 0;
     cin >> n;
 
-    int dis[n + 1][n + 1];
+    long int dis[n+1][n+1];
 
-    // taking the input
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= n; j++)
         {
             int w;
             cin >> w;
-            dis[i][j] = w;
+
+            if (w == -1) dis[i][j] = INT_MAX;
+            else dis[i][j] = w;
         }
     }
-    
-    // floyd warshall algorithm
+
     for (int k = 1; k <= n; k++)
     {
         for (int i = 1; i <= n; i++)
         {
             for (int j = 1; j <= n; j++)
             {
+                if (dis[i][k] == INT_MAX || dis[k][j] == INT_MAX) continue;
+
                 if (dis[i][j] > dis[i][k] + dis[k][j])
                 {
                     dis[i][j] = dis[i][k] + dis[k][j];
-                }
+                } 
             }
         }
     }
-
-    // Printing the distance array
+    
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= n; j++)
         {
-            cout << dis[i][j] << " ";
+            if(dis[i][j] > max_distance) {
+                max_distance = dis[i][j];
+            }
         }
-        cout << endl;
     }
 
-
+    cout << max_distance;
 
     return 0;
 }
-
