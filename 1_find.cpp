14f820211da7f19cbb0e43adbd9ce186a22c76ec
Ibnul Ferdous <ibnul04@gmail.com>
2023-08-30 09:50:18 +0600

    First commit

---
+++ 1_find.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Parent array
+int parent[8] = {-1, -1, 1, 1, 6, 4, -1, -1};
+
+int find_leader(int node)
+{
+    while (parent[node] != -1)
+    {
+        node = parent[node];
+    }
+
+    return node;
+}
+
+int main(void)
+{
+    int leader = find_leader(1);
+
+    cout << leader;
+
+    return 0;
+}
